4da4842e36f87180701b214bf245aa304ef90da0
My Own <hasanemail24@gmail.com>
2025-07-26 23:58:59 +0600

    ImageUploded

--- BookingRoom.Web/Controllers/VillaController.cs
+++ BookingRoom.Web/Controllers/VillaController.cs
@@ -8,9 +8,11 @@ namespace BookingRoom.Web.Controllers
     public class VillaController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public VillaController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public VillaController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -31,6 +33,20 @@ namespace BookingRoom.Web.Controllers
             }
             if (ModelState.IsValid)
             {
+                if(obj.Image != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(obj.Image.FileName);
+                    string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"Images/VillaImage");
+                    using var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create);
+                    obj.Image.CopyTo(fileStream);
+
+                    obj.ImageUrl = @"\Images\VillaImage\" + fileName;
+
+                }
+                else
+                {
+                    obj.ImageUrl = "https://placehold.co/600x400";
+                }
                 _unitOfWork.Villa.Add(obj);
                 _unitOfWork.Save();
                 TempData["success"] = "Villa created successfully.";
