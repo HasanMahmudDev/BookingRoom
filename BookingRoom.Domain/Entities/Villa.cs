aa8093b13e509b6c58672b78bd478f5577e0917d
My Own <hasanemail24@gmail.com>
2025-07-30 15:47:58 +0600

    hasan

--- BookingRoom.Domain/Entities/Villa.cs
+++ BookingRoom.Domain/Entities/Villa.cs
@@ -11,6 +11,7 @@ namespace BookingRoom.Domain.Entities
 {
     public class Villa
     {
+        //Hasan
         public int Id { get; set; }
         [MaxLength(50)]
         public required string Name { get; set; }
